17c7b5373e505cc9fd46157597ade6a311feb78f
Nadanai Kurairat <614259015@webmail.npru.ac.th>
2020-09-28 15:18:04 +0700

    three commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = require('./routes/index');
 const hbs = require('express-handlebars');
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
