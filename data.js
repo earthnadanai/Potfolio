5ed67d34e9524e34414b8425e35421bc84300401
Nadanai Kurairat <614259015@webmail.npru.ac.th>
2020-10-04 15:51:49 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,25 @@
+const data = {
+    "name":"Nadanai Kurairat",
+    "work":"Studen",
+    "aboutme":{
+        "FullName":"Nadanai Kurairat",
+        "Phone":"(+66)0951126224",
+        "Email":"614259015@webmail.npru.ac.th",
+        "Website":"https://www.facebook.com/earthlove5555/",
+        "Address":" 99/20 swine 9. Suan Luang Subdistrict. Krathum Baen District. Samut Sakhon Province. 74110"
+    },
+    "education":{
+        "elementarylevel":"Ban Suan Luang School",
+        "highschoollevel":"Wiset Samutkhun School",
+        "universitydegree":"Nakhon Pathom Rajabhat University"
+    },
+    "workepxperience":"Make a website selling games",
+    "skill":{
+        "skill1":"Make a website",
+        "skill2":"Write html, js, php",
+        "skill3":"Editer",
+        "skill4":"Able to do database"
+    }
+
+};
+module.exports = data;
